535513c8114b4de6c081a863e29e9fb71933918c
ryuichi <ichiryu--west23@docomo.ne.jp>
2025-05-27 01:58:13 +0900

    first commit

---
+++ models/IntegrationModel.py
@@ -0,0 +1,131 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+#--------------------------------------------------------------------
+def split_into_kernels(image, kernel_size):
+    b, c, h, w = image.shape
+    assert (h % kernel_size == 0) and (w % kernel_size == 0), "Image size must be divisible by kernel size"
+    patches = image.unfold(2, kernel_size, kernel_size).unfold(3, kernel_size, kernel_size)
+    patches = patches.contiguous().view(b, c, -1, kernel_size, kernel_size)
+    patches = patches.permute(0, 2, 1, 3, 4)
+    return patches      #(b, n_patches, c, kernel_size, kernel_size)
+#--------------------------------------------------------------------
+class PMEncoder(nn.Module):
+    def __init__(self,input_dim,output_dim,device='cpu'):
+        super(PMEncoder,self).__init__()
+        phase = torch.rand(output_dim, input_dim) * 2 * np.pi - np.pi
+        modulus = torch.ones(output_dim, input_dim)
+
+        real_part = modulus * torch.cos(phase)
+        imag_part = modulus * torch.sin(phase)
+
+        self.B = torch.complex(real_part, imag_part).detach().to(device)
+        self.B.requires_grad = False
+        self.alpha = torch.rand(input_dim) * 0.5 + 0.5
+        self.alpha = self.alpha.detach().to(device)
+        self.alpha.requires_grad = False
+
+    def forward(self, x):
+        x = torch.exp(1j * self.alpha * x) 
+        x = x.T  
+        x = torch.matmul(self.B, x).T 
+        x = torch.abs(x)**2 
+        return x
+    
+class IMEncoder(nn.Module):
+    def __init__(self,input_dim,output_dim,device='cpu'):
+        super(IMEncoder,self).__init__()
+        self.B = nn.Parameter(torch.randn(output_dim, 
+                                          input_dim) * (input_dim))
+        self.B.requires_grad = False
+
+    def forward(self, x):
+        x = x.T  
+        x = torch.matmul(self.B, x).T 
+        x = torch.abs(x)**2 
+        return x
+
+class MZMEncoder(nn.Module):
+    def __init__(self,input_dim,output_dim,device='cpu'):
+        super(MZMEncoder,self).__init__()
+        phase = torch.rand(output_dim, input_dim) * 2 * np.pi - np.pi
+        modulus = torch.ones(output_dim, input_dim)
+
+        real_part = modulus * torch.cos(phase)
+        imag_part = modulus * torch.sin(phase)
+
+        self.B = torch.complex(real_part, imag_part).detach().to(device)
+        self.B.requires_grad = False
+        self.alpha = torch.rand(input_dim) * 0.5 + 0.5
+        self.alpha = self.alpha.detach().to(device)
+        self.alpha.requires_grad = False
+
+    def forward(self, x):
+        x = 1+torch.exp(1j * self.alpha * x) 
+        x = x.T  
+        x = torch.matmul(self.B, x).T 
+        x = torch.abs(x)**2 
+        return x
+
+class LIEncoder(nn.Module):
+    def __init__(self,input_dim,output_dim,device='cpu'):
+        super(LIEncoder,self).__init__()
+        self.B = nn.Parameter(torch.randn(output_dim, 
+                                          input_dim) * (input_dim))
+        self.B.requires_grad = False
+
+    def forward(self, x):
+        x = x.T  
+        x = torch.matmul(self.B, x).T  
+        return x
+#--------------------------------------------------------------------
+from .Classifiers import MLP_for_10
+#--------------------------------------------------------------------
+
+class Image10Classifier(nn.Module):#10クラスの画像用
+    def __init__(self, dataset,kernel_size,leverage,
+                 enc_type,cls_type,num_layer,fc):
+        super(Image10Classifier, self).__init__()
+        dataset_config = {
+            'mnist':     {'img_size': 28, 'channels': 1},
+            'cifar-10':  {'img_size': 32, 'channels': 3},
+            'fashion-mnist': {'img_size': 28, 'channels': 1},
+            'cifar-100': {'img_size': 32, 'channels': 3},
+        }
+        if dataset not in dataset_config:
+            raise ValueError(f"Unknown dataset: {dataset}")
+        self.img_size = dataset_config[dataset]['img_size']
+        self.channels = dataset_config[dataset]['channels']
+
+        self.kernel_size = kernel_size
+        kernel_in = self.channels*kernel_size**2
+        feat_dim = int(kernel_in/leverage)
+        encoders = {
+            'PM':PMEncoder,
+            'IM':IMEncoder,
+            'MZM':MZMEncoder,
+            'LI':LIEncoder
+        }
+        classifiers = {
+            'MLP':MLP_for_10
+        }
+        self.num_patches = (self.img_size//kernel_size)*(self.img_size//kernel_size)
+        potential_dim = self.num_patches * feat_dim
+        self.split = split_into_kernels 
+        self.encoder = encoders[enc_type](kernel_size,leverage,self.channels) 
+        self.bn = nn.BatchNorm1d(feat_dim)
+        self.classifier =  classifiers[cls_type](potential_dim,num_layer,fc)
+        
+    def forward(self, x):
+        b=x.size(0)
+        x = x.view(b, self.channels, self.img_size, self.img_size)
+        x = self.split(x, self.kernel_size)#(b, p, c, k, k)
+        x = x.reshape(b * self.num_patches,
+                      self.channels * self.img_size**2)
+        x = self.encoder(x) 
+        x = self.bn(x)
+        x = x.reshape(b, -1)
+        x = self.classifier(x)
+        return x
