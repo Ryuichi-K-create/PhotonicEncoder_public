dce38898f7ee14a995e20db210703f11d3ab5278
ryuichi <ryu-konishi@stu.kanazawa-u.ac.jp>
2025-08-20 19:55:45 +0900

    cinic10実験系

--- Runner/main_DEQ.py
+++ Runner/main_DEQ.py
@@ -32,15 +32,15 @@ save = False
 params = {
     'none':[0], #variable_param=noneの際は1回だけ繰り返す
     #data---------------------------------------------
-    'dataset': 'covtype', # 'mnist', 'cifar-10', 'cinic-10' , 'fashion-mnist'
-    'batch_size': 512, #64 MNIST, 100 CIFAR10, 100 CINIC10, 512 COVTYPE
+    'dataset': 'cinic-10', # 'mnist', 'cifar-10', 'cinic-10' , 'fashion-mnist'
+    'batch_size': 100, #64 MNIST, 100 CIFAR10, 100 CINIC10, 512 COVTYPE
 
     #Encoder_Model--------------------------------
     'enc_type': 'PM', # 'none', 'MZM', 'LI'
     'alpha': np.pi/2, 
     #位相変調機の感度[np.pi*2,np.pi, np.pi/2, np.pi/4, np.pi/8, np.pi/16],pi:-π~π
     #class_model--------------------------------------
-    'cls_type': 'MLP', # 'MLP' or 'CNN'
+    'cls_type': 'CNN', # 'MLP' or 'CNN'
     'num_layer': 2,
     'fc': 'relu',
     'dropout': 0.0,
@@ -53,7 +53,7 @@ params = {
     #param--------------------------------------------
     'num_try': 3,
     'max_epochs': 10,
-    'leverage': 9, #mnist:[1,2,4,8,16],cinic:[1,2,3,4,6,8,12,16,24,48] enc is not none
+    'leverage': 16, #mnist:[1,2,4,8,16],cinic:[1,2,3,4,6,8,12,16,24,48] enc is not none
     'kernel_size': 4,
 
     #anderson param-----------------------------------
